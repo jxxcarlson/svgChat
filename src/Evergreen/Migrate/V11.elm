111cb5cd1dffdccc863fca156c7cfb52e1c0a1bf
James Carlson <jxxcarlson@gmail.com>
2020-04-27 18:21:00 +0200

    Preparing for v11

---
+++ src/Evergreen/Migrate/V11.elm
@@ -0,0 +1,64 @@
+module Evergreen.Migrate.V11 exposing (..)
+
+
+import Evergreen.V11.Types as New
+import Lamdera.Migrations exposing (..)
+import Evergreen.V11.Types as Old exposing(DragState(..), Position)
+import Dict
+import Set
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelMigrated ( {
+        messages = []
+      , messageFieldContent = ""
+      , clientDict = Dict.empty
+      , clientId = Nothing
+      , isDragging = False
+      , dragState = Static {x = 50, y = 50}
+      , userHandle = "---"
+      , message = ""
+       }, Cmd.none)
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+      ModelMigrated ( {
+            messages = []
+           , clients = Set.empty
+           , clientDict = Dict.empty
+           , seed = old.seed
+        }, Cmd.none)
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+      case old of
+         Old.MessageFieldChanged str -> MsgOldValueIgnored
+         Old.MessageSubmitted -> MsgOldValueIgnored
+         Old.DragStart -> MsgOldValueIgnored
+         Old.DragMove _ -> MsgOldValueIgnored
+         Old.DragStop _ -> MsgOldValueIgnored
+         Old.GotUserHandle _ -> MsgOldValueIgnored
+         Old.JoinChat -> MsgOldValueIgnored
+         Old.LeaveChat -> MsgOldValueIgnored
+         Old.ClearChatRoom -> MsgOldValueIgnored
+         Old.Noop -> MsgOldValueIgnored
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    case old of
+       Old.ClientJoin _ -> MsgOldValueIgnored
+       Old.ClientLeave _ -> MsgOldValueIgnored
+       Old.InitClientDict -> MsgOldValueIgnored
+       Old.MsgSubmitted _ _ -> MsgOldValueIgnored
+       Old.UpdateClientDict _ _ -> MsgOldValueIgnored
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
