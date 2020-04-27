3cd0d68a8809f74c0b264bae48595c396b971345
James Carlson <jxxcarlson@gmail.com>
2020-04-27 09:04:44 +0200

    use random initial position

--- src/Client.elm
+++ src/Client.elm
@@ -20,8 +20,8 @@ newAttributes seed maxX maxY clientStatus =
     (k, seed4) = Random.step (Random.int 0 11) seed3
     (color, fontColor) = getColors k
   in
-    ({ x  = 0
-    , y = 0
+    ({ x  = x
+    , y = y
     , radius = 20
     , color = color
     , fontColor = fontColor
