572e98fb8da9ea9c63d41d6c5643f3bcf84c077a
James Carlson <jxxcarlson@gmail.com>
2020-04-30 04:36:06 +0200

    Preparing for v41

---
+++ src/Evergreen/Migrate/V41.elm
@@ -0,0 +1,61 @@
+module Evergreen.Migrate.V41 exposing (..)
+
+import Evergreen.V37.Types as Old
+import Evergreen.V41.Types as New
+import Lamdera.Migrations exposing (..)
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelUnchanged
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelUnchanged
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+   case old of
+          Old.MessageFieldChanged _ -> MsgOldValueIgnored
+          Old.MessageSubmitted -> MsgOldValueIgnored
+          Old.DragStart _ -> MsgOldValueIgnored
+          Old.DragMove _ -> MsgOldValueIgnored
+          Old.DragStop _ -> MsgOldValueIgnored
+          Old.GotUserHandle _ -> MsgOldValueIgnored
+          Old.GotPassword _ -> MsgOldValueIgnored
+          Old.GotRepeatedPassword _ -> MsgOldValueIgnored
+          Old.SignUp -> MsgOldValueIgnored
+          Old.JoinChat -> MsgOldValueIgnored
+          Old.LeaveChat -> MsgOldValueIgnored
+          Old.EnterSignUpMode -> MsgOldValueIgnored
+          Old.EnterSignInMode -> MsgOldValueIgnored
+          Old.EnterChatMode -> MsgOldValueIgnored
+          Old.Noop -> MsgOldValueIgnored
+          Old.DeleteMe -> MsgOldValueIgnored
+          Old.RequestClearAllUsers -> MsgOldValueIgnored
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+          case old of
+             Old.ClientJoin _ _ -> MsgOldValueIgnored
+             Old.MsgSubmitted _ _ -> MsgOldValueIgnored
+             Old.UpdateClientDict _ _ -> MsgOldValueIgnored
+             Old.ClientLeave _ -> MsgOldValueIgnored
+             Old.CheckClientRegistration _ _  -> MsgOldValueIgnored
+             Old.ClearAll  -> MsgOldValueIgnored
+             Old.DeleteUser _ -> MsgOldValueIgnored
+
+
+
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
