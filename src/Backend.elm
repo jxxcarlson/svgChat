ef979216a77380de695f2805a488012d9f08abff
James Carlson <jxxcarlson@jxxMBPro16.local>
2020-10-03 03:14:58 -0400

    Temporarily disable authentication: make the function Backend.userIsValid always return True

--- src/Backend.elm
+++ src/Backend.elm
@@ -234,17 +234,17 @@ userHandleAvailable name clientDict =
 
 
 userIsValid : String -> String -> ClientDict -> Bool
-userIsValid userHandle passwordHash clientDict =
-    let
-        id =
-            findClientIdByHandle userHandle clientDict
-    in
-    case Dict.get id clientDict of
-        Nothing ->
-            False
+userIsValid userHandle passwordHash clientDict = True
+    -- let
+    --     id =
+    --         findClientIdByHandle userHandle clientDict
+    -- in
+    -- case Dict.get id clientDict of
+    --     Nothing ->
+    --         False
 
-        Just attributes ->
-            attributes.passwordHash == passwordHash
+    --     Just attributes ->
+    --         attributes.passwordHash == passwordHash
 
 
 findClientIdByHandle : String -> ClientDict -> ClientId
